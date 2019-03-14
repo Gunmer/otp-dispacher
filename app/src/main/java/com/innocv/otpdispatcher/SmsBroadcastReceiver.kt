8ddc9c8cc63a48b8f6041e1bcbc09e43f86b1f38
Cristiam Sosa <csosaur@gmail.com>
2019-03-15 00:47:39 +0100

    Initial commit

---
+++ app/src/main/java/com/innocv/otpdispatcher/SmsBroadcastReceiver.kt
@@ -0,0 +1,30 @@
+package com.innocv.otpdispatcher
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.telephony.SmsMessage
+import android.util.Log
+
+class SmsBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        try {
+            val bundle = intent?.extras ?: throw RuntimeException("Bundle is NULL")
+            val pdusObjs = bundle.get("pdus") as? Array<*> ?: throw RuntimeException("PdusObjs is NULL")
+            val format = bundle.getString("format")
+
+            pdusObjs.forEach {
+                val byteArray = it as ByteArray
+                val message = SmsMessage.createFromPdu(byteArray, format)
+                val phoneNumber = message.displayOriginatingAddress
+                val messageBody = message.displayMessageBody
+
+                Log.d("SmsBroadcastReceiver", "$phoneNumber: $messageBody)")
+            }
+        } catch (e: Exception) {
+            Log.e("SmsBroadcastReceiver", e.localizedMessage, e)
+        }
+    }
+
+}
